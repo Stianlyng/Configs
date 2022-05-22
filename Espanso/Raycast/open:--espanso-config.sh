707b77225da20525bc93d65efedfd929241272b2
Stian Lyng <71536569+Stianlyng@users.noreply.github.com>
2022-05-22 14:53:16 +0200

    adz

---
+++ Espanso/Raycast/open:--espanso-config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open:  Espanso Config
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author Stian Lyng
+
+open -a "Visual Studio Code" "/Users/stianlyng/Library/Application Support/espanso/match/base.yml"
+
